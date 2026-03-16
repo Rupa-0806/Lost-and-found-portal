8e8992d82205bf1a2b312be8097578a9af4019e9
Rupa-0806 <vrupashree08@gmail.com>
2026-03-16 17:49:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Toggle Navbar on Mobile
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Smooth scroll
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
